d2feafa979a41e0240bcfce7731e6a85ce070474
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-18 00:05:40 -0700

    add code to handle multiple bases, push or unshift-seems to do the same thing

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,68 @@
 $("form").submit(function(event) {
   event.preventDefault();
-  var startNumber = parseInt($("#theNumber").val());
-  $("#results").text(convert(startNumber));
+  var theNumber = parseInt($("#theNumber").val());
+  var theBase = parseInt($("#base").val());
+  $("#results").text(convert(theNumber, theBase));
 });
 
-function convert(aNumber) {
-  //var aNumber = aNumber.toString(2);
+function convert(aNumber, theBase) {
+  alert(aNumber.toString(theBase));
   var stringNumber="";
-  while(aNumber>1) {
-    if(aNumber%2===0) {
-      stringNumber="0"+stringNumber;
-    } else {
-      stringNumber="1"+stringNumber;
+  var numberArray=[];
+  while(aNumber>theBase) {
+    switch(aNumber%theBase) {
+      case 2:
+        numberArray.unshift("2");
+        break;
+      case 3:
+        numberArray.unshift("3");
+        break;
+      case 4:
+        numberArray.unshift("4");
+        break;
+      case 5:
+        numberArray.unshift("5");
+        break;
+      case 6:
+        numberArray.unshift("6");
+        break;
+      case 7:
+        numberArray.unshift("7");
+        break;
+      case 8:
+        numberArray.unshift("8");
+        break;
+      case 9:
+        numberArray.unshift("9");
+        break;
+      case 10:
+        numberArray.unshift("a");
+        break;
+      case 11:
+        numberArray.unshift("b");
+        break;
+      case 12:
+        numberArray.unshift("c");
+        break;
+      case 13:
+        numberArray.unshift("d");
+        break;
+      case 14:
+        numberArray.unshift("e");
+        break;
+      case 15:
+        numberArray.unshift("f");
+        break;
+      default:
+        numberArray.unshift("0");
+        break;
     }
-    aNumber=aNumber/2;
-  }
-  stringNumber+=aNumber;
+     aNumber=Math.floor(aNumber/theBase);
+   }
+
+  numberArray.push(aNumber);
+  alert(numberArray);
+  stringNumber=numberArray.join("");
+  alert(stringNumber);
   return stringNumber;
 }
