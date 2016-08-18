cd2df68c06ca60c5bce03438617dcf4f8eb667bd
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-18 06:40:41 -0700

    not fix the problem yet

--- js/scripts.js
+++ js/scripts.js
@@ -9,52 +9,56 @@ function convert(aNumber, theBase) {
   alert(aNumber.toString(theBase));
   var stringNumber="";
   var numberArray=[];
-  while(aNumber>theBase) {
+  while(aNumber>=theBase) {
+    debugger;
     switch(aNumber%theBase) {
+      case 1:
+        numberArray.push("1");
+        break;
       case 2:
-        numberArray.unshift("2");
+        numberArray.push("2");
         break;
       case 3:
-        numberArray.unshift("3");
+        numberArray.push("3");
         break;
       case 4:
-        numberArray.unshift("4");
+        numberArray.push("4");
         break;
       case 5:
-        numberArray.unshift("5");
+        numberArray.push("5");
         break;
       case 6:
-        numberArray.unshift("6");
+        numberArray.push("6");
         break;
       case 7:
-        numberArray.unshift("7");
+        numberArray.push("7");
         break;
       case 8:
-        numberArray.unshift("8");
+        numberArray.push("8");
         break;
       case 9:
-        numberArray.unshift("9");
+        numberArray.push("9");
         break;
       case 10:
-        numberArray.unshift("a");
+        numberArray.push("a");
         break;
       case 11:
-        numberArray.unshift("b");
+        numberArray.push("b");
         break;
       case 12:
-        numberArray.unshift("c");
+        numberArray.push("c");
         break;
       case 13:
-        numberArray.unshift("d");
+        numberArray.push("d");
         break;
       case 14:
-        numberArray.unshift("e");
+        numberArray.push("e");
         break;
       case 15:
-        numberArray.unshift("f");
+        numberArray.push("f");
         break;
       default:
-        numberArray.unshift("0");
+        numberArray.push("0");
         break;
     }
      aNumber=Math.floor(aNumber/theBase);
