8484e6e2da92068c7264ede349c7a5b12229ed01
Caleb Paul and Karen Freeman-Smith <caleb+karen@>
2016-08-17 17:14:33 -0700

    first attempt at conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+$("form").submit(function(event) {
+  event.preventDefault();
+  var startNumber = parseInt($("#theNumber").val());
+  $("#results").text(convert(startNumber));
+});
+
+function convert(aNumber) {
+  //var aNumber = aNumber.toString(2);
+  var stringNumber="";
+  while(aNumber>1) {
+    if(aNumber%2===0) {
+      stringNumber="0"+stringNumber;
+    } else {
+      stringNumber="1"+stringNumber;
+    }
+    aNumber=aNumber/2;
+  }
+  stringNumber+=aNumber;
+  return stringNumber;
+}
